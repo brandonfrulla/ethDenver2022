51384e26a416369cda63d0e7820c52c8a26ff9f8
Mahmoud <mahmoudmustafadev@outlook.com>
2022-02-19 01:36:06 +0200

    creating base for the extension UI and  backend

---
+++ LeapExtension/js/content.js
@@ -0,0 +1,4 @@
+console.log('Chrome extension go?');
+//TODO for some reason media player is undefiend
+var mediaPlayer = document.getElementsByTagName('video')[0];
+console.log('Media Player is: ' + mediaPlayer);
